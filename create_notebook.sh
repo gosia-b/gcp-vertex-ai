47571677afd20b13dd2b09364c2d3f0d0fb94557
gosia-b <64652599+gosia-b@users.noreply.github.com>
2022-10-09 22:43:19 +0200

    Update create_notebook.sh

--- create_notebook.sh
+++ create_notebook.sh
@@ -1,6 +1,15 @@
+# IMPORTANT: this code finally didn't work - notebooks instance was "creating" endlessly.
+# What might have helped:
+# 1) Troubleshooting: click name of the notebook, then "Health" and follow the documentation for the "Unhealthy" part
+# 2) Stop and resume the VM (in Compute Engine)
+
 # Environment variables
 REGION=europe-west1
 ZONE=europe-west1-b
+PROJECT_ID=cloud4us-gcp-i67w0t0rvc5d3ufap
+SERVICE_ACCOUNT=sa-vertex  # name of the service account to be created
+
+gcloud config set project $PROJECT_ID
 
 # Enable APIs
 gcloud services enable aiplatform.googleapis.com  # Vertex AI API
@@ -21,12 +30,14 @@ gcloud compute routers nats create my-nat-gateway \
     --nat-all-subnet-ip-ranges \
     --region=$REGION
 
-# Create service account
-gcloud iam service-accounts create sa-vertex \
-    --display-name="Service account for testing Vertex AI" \
-    --role="roles/storage.admin"  # https://cloud.google.com/storage/docs/access-control/iam-roles
+# Create a service account
+gcloud iam service-accounts create $SERVICE_ACCOUNT \
+    --display-name="Service account for testing Vertex AI"
 
-# Create notebook
+# Grant the service account an IAM role on the project
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+    --member="serviceAccount:$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com" \
+    --role="roles/storage.admin"  # https://cloud.google.com/storage/docs/access-control/iam-roles
 
 # In case of creating the notebook in the console - because of the organization policy, make sure to:
 # - check "Turn on Secure Boot"
@@ -36,9 +47,10 @@ gcloud iam service-accounts create sa-vertex \
 # https://cloud.google.com/vertex-ai/docs/workbench/user-managed/shielded-vm
 
 gcloud compute instances create my-notebook \
+    --service-account=$SERVICE_ACCOUNT@$PROJECT_ID.iam.gserviceaccount.com \
     --image-project=deeplearning-platform-release \
     --image-family=common-cpu-notebooks-debian-10 \
-    --metadata="proxy-mode=service_account" \
+    --metadata="proxy-mode=service_account,enable-guest-attributes=TRUE" \
     --scopes=https://www.googleapis.com/auth/cloud-platform \
     --shielded-secure-boot \
     --zone=$ZONE \
